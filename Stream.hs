639abe2cad1fc3529313981ede51d5364126c115
Masahiro Honma <hiratara@cpan.org>
2014-09-14 15:21:53 +0900

    Wait for upstream data consumed
    This is an excersize of 4.2.1 of Parallel and Concurrent Programming
    in Haskell.

--- Stream.hs
+++ Stream.hs
@@ -22,6 +22,12 @@ import Control.DeepSeq
 data IList a
   = Nil
   | Cons a (IVar (IList a))
+  | Fork (Par ()) (IList a)
+
+type ForkSetting = (Int, Int)
+
+defaultForkSetting :: ForkSetting
+defaultForkSetting = (200, 100)
 
 type Stream a = IVar (IList a)
 -- >>
@@ -30,39 +36,93 @@ instance NFData a => NFData (IList a) where
 --  rnf Nil = r0
   rnf Nil = ()
   rnf (Cons a b) = rnf a `seq` rnf b
+  rnf (Fork _ (Cons a b)) = rnf a `seq` rnf b
 
 -- -----------------------------------------------------------------------------
 -- Stream operators
 
 -- <<streamFromList
 streamFromList :: NFData a => [a] -> Par (Stream a)
-streamFromList xs = do
+streamFromList = streamFromList' defaultForkSetting
+
+streamFromList' :: NFData a => ForkSetting -> [a] -> Par (Stream a)
+streamFromList' (chunkSize, forkPoint) xs = do
   var <- new                            -- <1>
-  fork $ loop xs var                    -- <2>
+  fork $ kick xs var                    -- <2>
   return var                            -- <3>
  where
-  loop [] var = put var Nil             -- <4>
-  loop (x:xs) var = do                  -- <5>
+  loop _ [] var _ = put var Nil   -- <4>
+  loop (1, _) (x:_) var (Just next) = put var (Cons x next)
+  loop (n, 0) (x:xs) var Nothing = do
+    tail <- new
+    nVar <- new
+    put var (Fork (kick (drop (n - 1) xs) nVar)
+                  (Cons x tail))
+    loop (n - 1, -1) xs tail (Just nVar)
+  loop (n, k) (x:xs) var nVar = do      -- <5>
     tail <- new                         -- <6>
     put var (Cons x tail)               -- <7>
-    loop xs tail                        -- <8>
+    loop (n - 1, k - 1) xs tail nVar    -- <8>
+  kick xs var = loop (chunkSize, forkPoint) xs var Nothing
 -- >>
 
 -- <<streamMap
 streamMap :: NFData b => (a -> b) -> Stream a -> Par (Stream b)
-streamMap fn instrm = do
+streamMap = streamMap' defaultForkSetting
+
+streamMap' :: NFData b => ForkSetting -> (a -> b) -> Stream a -> Par (Stream b)
+streamMap' (chunkSize, forkPoint) fn instrm = do
   outstrm <- new
-  fork $ loop instrm outstrm
+  fork $ kick instrm outstrm
   return outstrm
  where
-  loop instrm outstrm = do
+  loop (1, _) instrm outstrm (Just next) = do
+    ilst <- get instrm
+    case ilst of
+      Nil -> put outstrm Nil
+      Cons h tail -> do
+        newtl <- new
+        put next (tail, newtl)
+        put outstrm (Cons (fn h) newtl)
+      Fork kickForking (Cons h tail) -> do
+        fork kickForking
+        newtl <- new
+        put next (tail, newtl)
+        put outstrm (Cons (fn h) newtl)
+  loop (n, 0) instrm outstrm Nothing = do
+    ilst <- get instrm
+    case ilst of
+      Nil -> put outstrm Nil
+      Cons h t -> do
+        newtl <- new
+        nextstrm <- new
+        put outstrm (Fork (kick' nextstrm)
+                          (Cons (fn h) newtl))
+        loop (n - 1, -1) t newtl (Just nextstrm)
+      Fork kickForking (Cons h t) -> do
+        fork kickForking
+        newtl <- new
+        nextstrm <- new
+        put outstrm (Fork (kick' nextstrm)
+                          (Cons (fn h) newtl))
+        loop (n - 1, -1) t newtl (Just nextstrm)
+  loop (n, k) instrm outstrm nextstrm = do
     ilst <- get instrm
     case ilst of
       Nil -> put outstrm Nil
       Cons h t -> do
         newtl <- new
         put outstrm (Cons (fn h) newtl)
-        loop t newtl
+        loop (n - 1, k - 1) t newtl nextstrm
+      Fork kickForking (Cons h t) -> do
+        fork kickForking
+        newtl <- new
+        put outstrm (Cons (fn h) newtl)
+        loop (n - 1, k - 1) t newtl nextstrm
+  kick instrm outstrm = loop (chunkSize, forkPoint) instrm outstrm Nothing
+  kick' var = do
+    (instrm, outstrm) <- get var
+    kick instrm outstrm
 -- >>
 
 
@@ -75,6 +135,7 @@ streamFold fn !acc instrm = do
   case ilst of
     Nil      -> return acc
     Cons h t -> streamFold fn (fn acc h) t
+    Fork kick (Cons h t) -> fork kick >> streamFold fn (fn acc h) t
 -- >>
 
 streamFilter :: NFData a => (a -> Bool) -> Stream a -> Par (Stream a)
